e04b5049a18da22aaef6670855891e4dfbc95e8e
Niels Tanis <nielstanis@live.nl>
2022-04-29 09:42:32 +0100

    fixed start

---
+++ Start/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello NDC Security 2022!");
+
+            DocumentProcessor.Processor proc = new DocumentProcessor.Processor();
+            //var result = await proc.ProcessDocumentAsync("Docs/schedule.pdf","schedule2022.pdf");
+            var result = await proc.ProcessDocumentAsync("https://ndc-security.com/","Output/ndcsecurity.html");
+            
+            Console.WriteLine("Done...");
+        }
+    }
+}
