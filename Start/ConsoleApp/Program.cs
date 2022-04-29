098b9292bee97afb8dd5cc74601b4714e1fef17b
Niels Tanis <nielstanis@live.nl>
2022-04-29 09:44:12 +0100

    fixed start

--- Start/ConsoleApp/Program.cs
+++ Start/ConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp
             Console.WriteLine("Hello NDC Porto 2022!");
 
             DocumentProcessor.Processor proc = new DocumentProcessor.Processor();
-            var result = await proc.ProcessDocumentAsync("Docs/schedule.pdf","schedule2022.pdf");
+            var result = await proc.ProcessDocumentAsync("Docs/schedule.pdf","Output/schedule2022.pdf");
             //var result = await proc.ProcessDocumentAsync("https://ndcporto.com/","Output/ndcporto.html");
             
             Console.WriteLine("Done...");
