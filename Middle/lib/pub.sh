7da04da3591ebf0e5c8288980e025cfa03ea4b90
Niels Tanis <nielstanis@live.nl>
2022-04-29 09:53:55 +0100

    remove native MacOSX lib from pub, because confusion

--- Middle/lib/pub.sh
+++ Middle/lib/pub.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 rm -rf ../Pub
 dotnet publish -c debug --self-contained -o ../Pub -r osx-x64
+
+#now delete libSystem.Native so it's forced to use shared one (change since .NET6)
+rm ../Pub/libSystem.Native.dylib 
